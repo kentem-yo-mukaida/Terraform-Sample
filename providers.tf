eade79fae619ce7199852ac47d146c7acf756a7d
yo-mukaida <yo-mukaida@kentem.co.jp>
2023-07-03 19:39:36 +0900

    修正？

--- providers.tf
+++ providers.tf
@@ -21,8 +21,8 @@ variable "ARM_CLIENT_SECRET" {}
 provider "azurerm" {
   features {}
 
-  subscription_id = "${var.ARM_SUBSCRIPTION_ID}"
-  tenant_id       = "${var.ARM_TENANT_ID}"
-  client_id       = "${var.ARM_CLIENT_ID}"
-  client_secret   = "${var.ARM_CLIENT_SECRET}"
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  tenant_id       = var.ARM_TENANT_ID
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
 }
